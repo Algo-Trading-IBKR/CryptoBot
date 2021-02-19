70320a9ec910762c171d211d70fae68879d02405
Yente De Wael <yente.de.wael@student.howest.be>
2021-02-19 22:41:59 +0100

    get 24 hour change

---
+++ Tests/Change24Hours.py
@@ -0,0 +1,23 @@
+#region imports
+import time, talib
+import pandas as pd
+import config
+from binance.client import Client
+from binance.websockets import BinanceSocketManager
+from binance.enums import *
+import json
+
+
+#endregion
+#region variables
+API_KEY = "hKclENu75kFYky6xVXxHqVNzdzOQc0RAZ5kElzJggmF3N2EQDn4XjP2XPhHtbxjI"
+SECRET_KEY = "2albG5h0FVjUpvIjg1aQvoVXH6sDgUvWsMmyzkajjaPkIrHaKUYUVgvraC86a7iS"
+
+client = Client(API_KEY, SECRET_KEY)
+
+
+
+tickers = client.get_ticker(symbol='BNBBTC')
+# avg_price = client.get_avg_price(symbol='BNBBTC')
+
+print(tickers)
