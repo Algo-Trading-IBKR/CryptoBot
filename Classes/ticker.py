fe88104efc8893e423c1ffbe472f5807efbdec5b
Yente De Wael <yente.de.wael@student.howest.be>
2021-02-21 23:57:46 +0100

    added variables

--- Classes/ticker.py
+++ Classes/ticker.py
@@ -8,8 +8,10 @@ class Ticker:
         self.closes = []
 
         self.has_position = False
-        # self.stop_loss = 
-        # self.take_profit =
+        self.buy_price = 0
+        self.stop_loss = 0
+        self.take_profit = 0
+        self.order_succeeded = False
 
     def get_highs(self):
         print(self.highs)
