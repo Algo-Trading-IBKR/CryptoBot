2ca1a9fd2eca1f08906eff940a5c319c5c1edbcf
Yente De Wael <yente.de.wael@student.howest.be>
2021-03-22 00:31:04 +0100

    added round down function

--- Tests/Margin/MarginInfo.py
+++ Tests/Margin/MarginInfo.py
@@ -14,6 +14,7 @@ API_KEY = "SZLgVpzpnAoroc2YTSJ3hVJmBIH4tANSBO5I0dobNw4ORbFdysAoJVP0tR9pE9CF"
 SECRET_KEY = "YHyoGrthBZoqg0gEcVuvwkuRul7L4NxcqjaxHXDM0cbVjRPkN6S7eQCzyeywfVgu"
 client = Client(API_KEY, SECRET_KEY)
 
+money = 100
 
 pairs = ["BTCUSDT", "ETHUSDT", "DOTUSDT", "ADAUSDT", "SFPUSDT","FTMUSDT","XEMUSDT","NPXSUSDT","KSMUSDT","ONTUSDT","RLCUSDT","ONGUSDT","OMGUSDT","MANAUSDT","XRPUSDT","BNBUSDT","CAKEUSDT","REEFUSDT", "TROYUSDT", "DODOUSDT" ,"ENJUSDT", "VTHOUSDT","AXSUSDT","CRVUSDT","UNFIUSDT"]
 
@@ -33,10 +34,10 @@ pairs = ["BTCUSDT", "ETHUSDT", "DOTUSDT", "ADAUSDT", "SFPUSDT","FTMUSDT","XEMUSD
 
 info = client.get_isolated_margin_account(symbols="LITUSDT")
 # x =  json.loads(info)
-print(info["assets"][0]["baseAsset"])
-print(info["assets"][0]["quoteAsset"])
-print(info["assets"][0]["liquidatePrice"])
-print(info["assets"][0]["marginRatio"])
+# print(info["assets"][0]["baseAsset"])
+# print(info["assets"][0]["quoteAsset"])
+# print(info["assets"][0]["liquidatePrice"])
+# print(info["assets"][0]["marginRatio"])
 
 
 
@@ -45,24 +46,44 @@ balance = client.get_asset_balance(asset='USDT')
 # info = client.get_margin_account()
 # info = client.get_isolated_margin_account()
 
-amount = 1.0989
+amount = 1.2879
 can_sell = amount - (amount * 0.002) 
 
 
 
-symbol_info = client.get_symbol_info('BTCUSDT')
-step_size = 0.0
-for f in symbol_info['filters']:
-  if f['filterType'] == 'LOT_SIZE':
-    step_size = float(f['stepSize'])
+
+# print(symbol_info)
+# step_size = 0.0
+# for p in pairs:
+#   symbol_info = client.get_symbol_info(p)
+#   for f in symbol_info['filters']:
+#     if f['filterType'] == 'LOT_SIZE':
+#       step_size = float(f['stepSize'])
+#       precision = int(round(-math.log(step_size, 10), 0))
+      
+#       print(p, " precision: ",precision)
+
+precision = 2
+quantity = float(round(amount, precision))
+
+print(quantity)
 
 
-precision = int(round(-math.log(step_size, 10), 0))
-# quantity = float(round(1.0989, 3))
-print("precision: ",precision)
 
+def get_amount(number:float, decimals:int=2):
+# https://kodify.net/python/math/round-decimals/#round-decimal-places-down-in-python
+    if not isinstance(decimals, int):
+        raise TypeError("decimal places must be an integer")
+    elif decimals < 0:
+        raise ValueError("decimal places has to be 0 or more")
+    elif decimals == 0:
+        return math.floor(number)
 
+    factor = 10 ** decimals
+    return math.floor(number * factor) / factor
 
+test = get_amount(number=amount*10, decimals=precision)
+print(test)
 # info = client.get_exchange_info() 
 # print(balance)
 # print("quantity: ", quantity)
