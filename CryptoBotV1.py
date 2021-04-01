f32fdd147295a066b1fa3ed4f1df70b3b1f784d7
Yente De Wael <yente.de.wael@student.howest.be>
2021-04-01 23:18:09 +0200

    added stop loss for pirammiding use

--- CryptoBotV1.py
+++ CryptoBotV1.py
@@ -210,48 +210,65 @@ def process_m_message(msg):
                     last_rsi = rsi[-1]
                     last_mfi = mfi[-1]
 
-                    # verkopen
-                    # if symbol.has_position:
-                    #     current_price = symbol.closes[-1]
-                    #     current_high = symbol.highs[-1]
-                        # order_succeeded = False
+                    verkopen
+                    if symbol.has_position:
+                        current_price = symbol.closes[-1]
+                        current_high = symbol.highs[-1]
+                        order_succeeded = False
 
-                    #     # sell with a profit
-                    #     if current_high > float(symbol.take_profit):
-                    #         print(Fore.RED + f"{name} sold with a profit.")
-                    #         symbol.money += symbol.amount * symbol.take_profit
-                    #         symbol.log_sell(profit=True ,price=symbol.take_profit ,ticker=name ,amount=symbol.amount ,money=symbol.money)
-                    #         order_succeeded = True
-                    #         symbol.amount = 0
-                    #         if order_succeeded:
-                    #             symbol.has_position = False
-                    #             order_succeeded = False
+                        # # sell with a profit
+                        # if current_high > float(symbol.take_profit):
+                        #     print(Fore.RED + f"{name} sold with a profit.")
+                        #     symbol.money += symbol.amount * symbol.take_profit
+                        #     symbol.log_sell(profit=True ,price=symbol.take_profit ,ticker=name ,amount=symbol.amount ,money=symbol.money)
+                        #     order_succeeded = True
+                        #     symbol.amount = 0
+                        #     if order_succeeded:
+                        #         symbol.has_position = False
+                        #         order_succeeded = False
 
                         
-                    #     # sell with a loss of initiate piramiding
-                    #     if current_price < float(symbol.stop_loss):
-                    #         # piramidding
-                    #         if float(symbol.money) > 20:
-                    #             print(Fore.RED  +f"{name} price dropped under the stop loss, buy a second time.")
-                    #             piramidding_amount = symbol.money/current_price
-                    #             symbol.average_price = (symbol.amount*symbol.average_price + piramidding_amount*current_price) / (symbol.amount+piramidding_amount)
-                    #             symbol.log_buy(amount=piramidding_amount ,buy_price=current_price, ticker=name, money=symbol.money)
-                    #             symbol.amount += piramidding_amount
-                    #             symbol.stop_loss = get_low(symbol.ticker)
-                    #             if float(symbol.stop_loss) > (symbol.average_price - (symbol.average_price*0.05)):
-                    #                 symbol.stop_loss = symbol.average_price - (symbol.average_price*0.05)
-                    #             symbol.take_profit = symbol.average_price * 1.012
-                    #             symbol.money -= current_price * piramidding_amount
-                    #             order_succeeded = True
+                        # sell with a loss of initiate piramiding
+                        if current_price < float(symbol.stop_loss):
+                            # piramidding
+                            asset = client.get_isolated_margin_account(symbols=name)
+                            free_asset, borrowed_asset = asset["assets"][0]["baseAsset"]["free"], asset["assets"][0]["baseAsset"]["borrowed"]
+                            free_quote, borrowed_quote = asset["assets"][0]["quoteAsset"]["free"], asset["assets"][0]["quoteAsset"]["borrowed"]
+                            if free_quote >= 11:
+                                print(Fore.RED  +f"{name} price dropped under the stop loss, buy a second time.")
 
-                    #             if order_succeeded:
-                    #                 print(Fore.GREEN + f"SECOND BUY: {name} bought for {current_price} dollar. Stop loss at {symbol.stop_loss} and take profit at {symbol.take_profit}")
-                    #                 try:
-                    #                     response = clickatell.sendMessage(to=phone_numbers, message=f"SECOND BUY: {name} bought for {current_price} dollar.")
-                    #                 except Exception as e:
-                    #                     pass
-                    #                 symbol.has_position = True
-                    #                 order_succeeded = False
+
+
+
+
+
+
+
+
+
+
+
+                            if float(symbol.money) > 20:
+                                
+                                piramidding_amount = symbol.money/current_price
+                                symbol.average_price = (symbol.amount*symbol.average_price + piramidding_amount*current_price) / (symbol.amount+piramidding_amount)
+                                symbol.log_buy(amount=piramidding_amount ,buy_price=current_price, ticker=name, money=symbol.money)
+                                symbol.amount += piramidding_amount
+                                symbol.stop_loss = get_low(symbol.ticker)
+                                if float(symbol.stop_loss) > (symbol.average_price - (symbol.average_price*0.05)):
+                                    symbol.stop_loss = symbol.average_price - (symbol.average_price*0.05)
+                                symbol.take_profit = symbol.average_price * 1.012
+                                symbol.money -= current_price * piramidding_amount
+                                order_succeeded = True
+
+                                if order_succeeded:
+                                    print(Fore.GREEN + f"SECOND BUY: {name} bought for {current_price} dollar. Stop loss at {symbol.stop_loss} and take profit at {symbol.take_profit}")
+                                    try:
+                                        response = clickatell.sendMessage(to=phone_numbers, message=f"SECOND BUY: {name} bought for {current_price} dollar.")
+                                    except Exception as e:
+                                        pass
+                                    symbol.has_position = True
+                                    order_succeeded = False
 
                     #         # loss
                     #         else:
@@ -277,9 +294,9 @@ def process_m_message(msg):
                             transaction = transfer_to_isolated(asset="USDT", ticker=symbol.ticker, amount=22)
                             symbol.average_price = symbol.closes[-1] #get the wanted buy price 
                             symbol.amount = get_amount((22/2)/symbol.average_price, symbol.precision) #get amount the bot could buy
-                            # symbol.stop_loss = get_low(symbol.ticker) #get a stop loss
-                            # if float(symbol.stop_loss) > (symbol.average_price - (symbol.average_price*0.03)):
-                            #     symbol.stop_loss = symbol.average_price - (symbol.average_price*0.03)
+                            symbol.stop_loss = get_low(symbol.ticker) #get a stop loss
+                            if float(symbol.stop_loss) > (symbol.average_price - (symbol.average_price*0.03)):
+                                symbol.stop_loss = symbol.average_price - (symbol.average_price*0.03)
                             symbol.take_profit = symbol.average_price * 1.011 #get a take profit amount
 
                             order_succeeded = send_order(side=SIDE_BUY , quantity=symbol.amount*symbol.margin_ratio, ticker=symbol,price=symbol.average_price,order_type=ORDER_TYPE_LIMIT,isolated=True,side_effect="MARGIN_BUY")
@@ -288,7 +305,7 @@ def process_m_message(msg):
                             if order_succeeded:
                                 # log buy 
                                 symbol.log_buy(amount=symbol.amount ,buy_price=symbol.average_price, ticker=name, money=symbol.money)
-                                print(Fore.GREEN + f"{name} bought for {symbol.average_price} dollar. rsi: {last_rsi} mfi: {last_mfi}. Stop loss at {symbol.stop_loss} and take profit at {symbol.take_profit}")
+                                print(Fore.GREEN + f"{name} bought for {symbol.average_price} dollar. rsi: {last_rsi} mfi: {last_mfi}. pirammiding at {symbol.stop_loss} and take profit at {symbol.take_profit}")
                                 symbol.has_position = True
                                 order_succeeded = False
 
@@ -333,11 +350,12 @@ def callback_isolated_accounts(msg):
         # when the take profit order is reached
         if side == "SELL" and execution_type == "TRADE":
             asset = client.get_isolated_margin_account(symbols=name)
-            free_asset = asset["assets"][0]["baseAsset"]["free"]
-            free_quote = asset["assets"][0]["quoteAsset"]["free"]
-            # put transaction here
-            transaction_asset = transfer_to_spot(asset=name[:-4], ticker=symbol.ticker, amount=free_asset)
-            transaction_quote = transfer_to_spot(asset="USDT", ticker=symbol.ticker, amount=free_quote)
+            free_asset, borrowed_asset = asset["assets"][0]["baseAsset"]["free"], asset["assets"][0]["baseAsset"]["borrowed"]
+            free_quote, borrowed_quote = asset["assets"][0]["quoteAsset"]["free"], asset["assets"][0]["quoteAsset"]["borrowed"]
+            if borrowed_asset == 0 and borrowed_quote == 0: 
+                #transaction to spot
+                transaction_asset = transfer_to_spot(asset=name[:-4], ticker=symbol.ticker, amount=free_asset)
+                transaction_quote = transfer_to_spot(asset="USDT", ticker=symbol.ticker, amount=free_quote)
             
 
 
