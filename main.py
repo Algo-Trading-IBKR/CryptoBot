031f6215c5e0faee0d91f66074c481806f2dfe9f
Yimura <andreas.maerten@scarlet.be>
2021-05-08 17:53:19 +0200

    feat(Main): Start when called directly by python

--- main.py
+++ main.py
@@ -10,5 +10,6 @@ signal.signal(signal.SIGTERM, cryptobot.shutdown)
 async def main():
     await cryptobot.start()
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
