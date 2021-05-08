2bed16de7573951ac75af6374844ee5b84e09063
Yimura <andreas.maerten@scarlet.be>
2021-05-08 17:57:31 +0200

    wip(CryptoBot): Added Coin, OrderManager and Wallet setup

--- src/cryptobot.py
+++ src/cryptobot.py
@@ -1,11 +1,18 @@
 import asyncio
 from binance import AsyncClient
-from src.util.logger import Logger
+from src.util.logger import Log
 from src.util.util import load_json
+from .coin import Coin
+from .order_manager import OrderManager
+from .wallet import Wallet
 
 class CryptoBot:
     def __init__(self):
         self._config = load_json('./configs/config.json')
+
+    @property
+    def client(self):
+        return self._client
 
     @property
     def config(self):
@@ -17,12 +24,23 @@ class CryptoBot:
     async def start(self):
         self._active = self._config["configs"][self._config["active_config"]]
 
-        Logger.info('BOT', f"Using active config {self._active['name']}")
-        Logger.info('BOT', f"Using {len(self._config['pairs'])} coin pairs.")
+        Log.info('BOT', f"Using active config {self._active['name']}")
+        Log.info('BOT', f"Using {len(self._config['pairs'])} coin pairs.")
 
-        self._client = await AsyncClient.create(self._active["key"], self._active["secret"])
+        self._client = await AsyncClient.create(api_key=self._active["key"], api_secret=self._active["secret"])
 
-    def shutdown(self):
+        status = await self._client.get_system_status()
+        Log.info('BOT', f"System status: {status['msg']}")
+
+        self._coins = [Coin(self, symbol_pair) for symbol_pair in self._config["pairs"]]
+        self._order_manager = OrderManager(self)
+        self._wallet = Wallet(self)
+
+        for coin in self._coins:
+            coin.start()
+
+        await self._client.close_connection()
+
+    async def shutdown(self):
         # gracefull shutdown, close positionts etc
-
-        pass
+        await self._client.close_connection()
