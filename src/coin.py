613720dcd5c9eea9e872c0377b5cefc957b18525
Yimura <andreas.maerten@scarlet.be>
2021-05-08 17:56:41 +0200

    wip(Coin): Added threading and some init params

--- src/coin.py
+++ src/coin.py
@@ -1,4 +1,11 @@
-class Coin():
-    def __init__(self):
+import threading
 
+class Coin(threading.Thread):
+    def __init__(self, bot, symbol_pair : str):
+        threading.Thread.__init__(self)
+
+        self.bot = bot
+        self.symbol_pair = symbol_pair
+
+    def run(self):
         pass
