d944dfa3ca4e22cb2719e96b8a478e8b1586a3eb
Yimura <andreas.maerten@scarlet.be>
2021-05-11 10:17:20 +0200

    refactor(Wallet): Changed logs

--- src/wallet.py
+++ src/wallet.py
@@ -9,7 +9,7 @@ class Wallet:
     async def transfer_to_isolated(self, symbol, symbol_pair, amount):
         t = self.bot.client.transfer_spot_to_isolated_margin(asset=symbol, symbol=symbol_pair, amount=amount)
 
-        self.bot.log.verbose('WALLET', f'Transfer {amount} {symbol_pair} to isolated')
+        self.bot.log.verbose('WALLET', f'Transfer {amount} {symbol} to isolated')
 
         await self.update_money()
 
@@ -18,7 +18,7 @@ class Wallet:
     async def transfer_to_spot(self, symbol, symbol_pair, amount):
         t = await self.bot.client.transfer_isolated_margin_to_spot(asset=symbol, symbol=symbol_pair, amount=amount)
 
-        self.bot.log.verbose('WALLET', f'Transfer {amount} {symbol_pair} to spot')
+        self.bot.log.verbose('WALLET', f'Transfer {amount} {symbol} to spot')
 
         await self.update_money()
 
