4c101f1c3d3b1e0c42fb71e621075254160bea88
joren vanGoethem <jorenvangoethem@hotmail.com>
2021-05-27 15:11:51 +0100

    remove influx

--- main.py
+++ main.py
@@ -13,24 +13,19 @@ signal.signal(signal.SIGTERM, cryptobot.shutdown)
 async def main():
     active_config = os.environ.get('ACTIVE_CONFIG')
     mongo_connect_url = os.environ.get('MONGODB_URL')
-    influx_org = os.environ.get('INFLUX_ORG')
-    influx_token = os.environ.get('INFLUX_TOKEN')
-    influx_url = os.environ.get('INFLUX_URL')
 
-    influx_config = [influx_org, influx_token, influx_url]
+    Mongo = MongoClient(mongo_connect_url)
 
-    client = MongoClient(mongo_connect_url)
-
-    indicators = client.cryptobot.indicators.find({})
-    symbol_pairs = client.cryptobot.symbol_pairs.find({ "active": True })
-    user_config = client.cryptobot.users.find_one({ "active": True, "name": active_config })
+    indicators = Mongo.cryptobot.indicators.find({})
+    symbol_pairs = Mongo.cryptobot.symbol_pairs.find({ "active": True })
+    user_config = Mongo.cryptobot.users.find_one({ "active": True, "name": active_config })
 
     cryptobot.log.set_log_group(active_config)
 
     if user_config == None:
         return cryptobot.log.info('MAIN', f'User config "{active_config}" is disabled')
 
-    await cryptobot.start(user_config, symbol_pairs, indicators, influx_config)
+    await cryptobot.start(user_config, symbol_pairs, indicators, Mongo)
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
